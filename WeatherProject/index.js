865678bdcf98803e8b3285f663d37e8739d81b7f
Nishijama <nathanplociennik@gmail.com>
2022-10-07 00:22:00 +0200

    finished WeatherProject

--- WeatherProject/index.js
+++ WeatherProject/index.js
@@ -3,31 +3,36 @@ const app = express();
 const bodyParser = require("body-parser")
 const https = require("node:https");
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 const apiKey = 'cbf79b77dbb6f692180fd6e8e6c7b0b6'
 
 app.get("/", (req, res) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Poznan&appid=${apiKey}&units=metric`
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body.city);
+    let query = req.body.city
+    getWeatherData(query, res);
+})
+
+function getWeatherData(query, res) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`
     https.get(url, (response) => {
         response.on("data", (data) => {
             const weatherData = JSON.parse(data);
             const temp = String(weatherData.main.temp)
-            console.log(temp);
-            // res.send(temp)
+            const description = String(weatherData.weather[0].description)
+            const icon = weatherData.weather[0].icon;
+            const image = `http://openweathermap.org/img/wn/${icon}.png`
+            res.write(`<h1>The weather in ${query} is ${description}</h1>`)
+            res.write(`<p>Temperature  ${temp} C </p>`)
+            res.write(`<img src=${image}>`)
+            res.send();
         })
     })
-    res.send("Server is up and running.")
-})
-
-// app.use(bodyParser.urlencoded({extended:false}));
-
-// //     res.sendFile(__dirname + "/index.html");
-// // })
-
-// app.post("/", (req, res) => {
-//     let city = req.body.city;
-//     let email = req.body.email;
-//     res.send(`<p>Registered user: <strong>${city}</strong></p><p>Registered email: <strong>${email}</strong></p>`);
-// })
+}
 
 app.listen(3000, () => {
     console.log("App running on port 3000");
